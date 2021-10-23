e97ed49247a9d3d6af48f873bf8c8098573db2ba
mmenager <maxence.menager@gmail.com>
2021-10-23 15:32:59 -0400

    add nvim conf

--- install.sh
+++ install.sh
@@ -8,3 +8,10 @@ echo "Configuring zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Configuring nvim"
+mkdir ~/nvim
+cp init.vim ~/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
