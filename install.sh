e5ec54b4f3724966768ed4ac64ffd47c90744a6d
mmenager <maxence.menager@gmail.com>
2021-10-23 15:05:42 -0400

    improve zsh install

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ cp .vimrc ~
 vim +PluginInstall +qall
 
 echo "Configuring zsh"
-apt install zsh
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
