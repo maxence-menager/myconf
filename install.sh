9ace8a9b171d89e061b4c555830c1da385556d40
menagerm <maxence.menager@gmail.com>
2021-10-23 20:44:12 +0200

    add zsh

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@ mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~
 vim +PluginInstall +qall
+
+echo "Configuring zsh"
+apt install zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~
